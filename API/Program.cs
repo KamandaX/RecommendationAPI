0edea5d7d3678127eb1d8f212dc0e058d858af7f
Gabrielius <gabrielius9915@gmail.com>
2020-03-04 13:47:30 +0200

    Update Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -7,7 +7,8 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            yeet();
+            CreateHostBuilder(args).Build().Run()
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
