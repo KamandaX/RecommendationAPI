eecee936280bb225861d995987cc0bb14f340c9e
Gabrielius Laurinavičius <gabrielius9915@gmail.com>
2020-04-27 16:33:10 +0300

    Separate data seeding to classes

---
+++ API/Models/Seed/PhoneSeed.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace API.Models.Seed
+{
+    public static class PhoneSeed
+    {
+        public static void EnsureCreated(ApiContext context)
+        {
+            context.Phones.AddRange(
+                new Phone
+                {
+                    ID = 1,
+                    Manufacturer = "Samsung",
+                    ModelName = "Galaxy S20",
+                    DisplayResolutionHorizontal = 1440,
+                    DisplayResolutionVertical = 3200,
+                    Price = 999M,
+                    LaunchDate = DateTime.Parse("2020-2-11"),
+                    ScreenSize = 6.2f,
+                    ProtectionRating = 8.5f,
+                    Processor = "Octa-core (2x2.73 GHz Mongoose M5 & 2x2.50 GHz Cortex-A76 & 4x2.0 GHz Cortex-A55) - Global\nOcta-core (1x2.84 GHz Kryo 585 & 3x2.42 GHz Kryo 585 & 4x1.8 GHz Kryo 585) - USA",
+                    Storage = 128,
+                    Ram = 8,
+                    BatterySize = 4000
+                },
+                new Phone
+                {
+                    ID = 2,
+                    Manufacturer = "Samsung",
+                    ModelName = "Galaxy S9",
+                    DisplayResolutionHorizontal = 1440,
+                    DisplayResolutionVertical = 2960,
+                    Price = 299M,
+                    LaunchDate = DateTime.Parse("2018-3-1"),
+                    ScreenSize = 5.8f,
+                    ProtectionRating = 8.0f,
+                    Processor = "Octa-core (4x2.7 GHz Mongoose M3 & 4x1.8 GHz Cortex-A55) - EMEA\nOcta-core (4x2.8 GHz Kryo 385 Gold & 4x1.7 GHz Kryo 385 Silver) - USA/LATAM, China",
+                    Storage = 128,
+                    Ram = 4,
+                    BatterySize = 3000
+                }
+            );
+        }
+    }
+}
---
+++ API/Models/Seed/QuestionOptionSeed.cs
@@ -0,0 +1,46 @@
+﻿namespace API.Models.Seed
+{
+    public static class QuestionOptionSeed
+    {
+        public static void EnsureCreated(ApiContext context)
+        {
+            context.QuestionOptions.AddRange(
+                new QuestionOption
+                {
+                    ID = 1,
+                    QuestionID = 1,
+                    OptionContent = "range",
+                    QuestionMultiplier = 0.5f
+                },
+                new QuestionOption
+                {
+                    ID = 2,
+                    QuestionID = 2,
+                    OptionContent = "stars",
+                    QuestionMultiplier = 0.5f
+                },
+                new QuestionOption
+                {
+                    ID = 3,
+                    QuestionID = 3,
+                    OptionContent = "stars",
+                    QuestionMultiplier = 0.5f
+                },
+                new QuestionOption
+                {
+                    ID = 4,
+                    QuestionID = 4,
+                    OptionContent = "stars",
+                    QuestionMultiplier = 0.5f
+                },
+                new QuestionOption
+                {
+                    ID = 5,
+                    QuestionID = 5,
+                    OptionContent = "stars",
+                    QuestionMultiplier = 0.5f
+                }
+            );
+        }
+    }
+}
