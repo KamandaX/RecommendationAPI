2f9276f6dbb0addd17bde9333cb71246afe65c39
Gabrielius Laurinavičius <gabrielius9915@gmail.com>
2020-05-02 18:56:34 +0300

    Fix URL

--- API/Controllers/QuestionsController.cs
+++ API/Controllers/QuestionsController.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class QuestionsController : ApiControllerBase
     {
