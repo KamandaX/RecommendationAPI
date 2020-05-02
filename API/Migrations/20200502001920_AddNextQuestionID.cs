9733e2bec380a36bce172b0a63af2eeda58193e3
Gabrielius Laurinavičius <gabrielius9915@gmail.com>
2020-05-02 18:34:15 +0300

    Create AddNextQuestionID migration

---
+++ API/Migrations/20200502001920_AddNextQuestionID.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class AddNextQuestionID : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NextQuestionID",
+                table: "QuestionOptions",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuestionOptions_NextQuestionID",
+                table: "QuestionOptions",
+                column: "NextQuestionID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_QuestionOptions_Questions_NextQuestionID",
+                table: "QuestionOptions",
+                column: "NextQuestionID",
+                principalTable: "Questions",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.NoAction);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_QuestionOptions_Questions_NextQuestionID",
+                table: "QuestionOptions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_QuestionOptions_NextQuestionID",
+                table: "QuestionOptions");
+
+            migrationBuilder.DropColumn(
+                name: "NextQuestionID",
+                table: "QuestionOptions");
+        }
+    }
+}
