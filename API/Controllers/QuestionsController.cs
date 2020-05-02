c2724144d700c1b2b0d9b4dc4695c5483d1cb5d0
Gabrielius Laurinavičius <gabrielius9915@gmail.com>
2020-05-02 18:35:24 +0300

    Add QuestionsController

---
+++ API/Controllers/QuestionsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using API.Models;
+using API.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ApiControllerBase
+    {
+        public QuestionsController(ApiContext context, Iserializer serializer, IErrorFormatter errorFormatter) :
+            base(context, serializer, errorFormatter)
+        { }
+
+        [HttpGet("{id=0}")]
+        public async Task<ActionResult<string>> GetQuestion(int id)
+        {
+            if (!IsValidApiRequest())
+            {
+                return ApiBadRequest();
+            }
+
+            try
+            {
+                var question = id == 0 ?
+                    await Context.Questions.Include(i => i.QuestionOptions).FirstOrDefaultAsync().ConfigureAwait(false) :
+                    await Context.Questions.Include(i => i.QuestionOptions).SingleOrDefaultAsync(i => i.ID == id).ConfigureAwait(false);   
+
+                if (question == default(Question))
+                    return ErrorFormatter.FormatError(500, "Json Error", "Not Found");
+
+                string json = Serializer.Encode(question);
+                return json;
+            }
+            catch (Exception ex)
+            {
+                return ErrorFormatter.FormatError(500, "Json Error", ex.Message);
+            }
+        }
+    }
+}
