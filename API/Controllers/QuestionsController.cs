c99f820115e201689cc4df054efb4a51fcda4d9b
wizacass <visak.pet0@gmail.com>
2020-05-10 17:03:00 +0300

    Send proper HTTP Status code alongside response

--- API/Controllers/QuestionsController.cs
+++ API/Controllers/QuestionsController.cs
@@ -12,32 +12,33 @@ namespace API.Controllers
     public class QuestionsController : ApiControllerBase
     {
         public QuestionsController(ApiContext context, Iserializer serializer, IErrorFormatter errorFormatter) :
-            base(context, serializer, errorFormatter)
-        { }
+            base(context, serializer, errorFormatter) { }
 
         [HttpGet("{id=0}")]
         public async Task<ActionResult<string>> GetQuestion(int id)
         {
             if (!IsValidApiRequest())
             {
-                return ApiBadRequest();
+                return ApiBadRequest("Invalid headers!");
             }
 
             try
             {
-                var question = id == 0 ?
-                    await Context.Questions.Include(i => i.QuestionOptions).FirstOrDefaultAsync().ConfigureAwait(false) :
-                    await Context.Questions.Include(i => i.QuestionOptions).SingleOrDefaultAsync(i => i.ID == id).ConfigureAwait(false);
+                var question = id == 0 ? 
+                    await Context.Questions.Include(i => i.QuestionOptions).FirstOrDefaultAsync()
+                        .ConfigureAwait(false) :
+                    await Context.Questions.Include(i => i.QuestionOptions).SingleOrDefaultAsync(i => i.ID == id)
+                        .ConfigureAwait(false);
 
                 if (question == default(Question))
-                    return ErrorFormatter.FormatError(404, "Json Error", "Not Found");
+                    return ApiNotFound("Question does not exist!");
 
                 string json = Serializer.Encode(question);
                 return json;
             }
             catch (Exception ex)
             {
-                return ErrorFormatter.FormatError(500, "Json Error", ex.Message);
+                return InternalServerError("Json error!", ex.Message);
             }
         }
     }
