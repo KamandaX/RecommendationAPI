42afc3f67c1878adc82498010058cba38116d148
Lukas Rakauskas <rakauskaslukas123@gmail.com>
2020-05-13 15:08:09 +0300

    Add User extension
    User extension is used to get user information from the jwt token provided.

---
+++ API/Extensions/UserExtension.cs
@@ -0,0 +1,21 @@
+﻿using API.Models;
+using System;
+using System.Security.Claims;
+
+namespace API.Extensions
+{
+    public static class UserExtension
+    {
+        public static User GetUserData(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+                throw new ArgumentNullException(nameof(principal));
+
+            return new User
+            {
+                Email = principal.FindFirst(ClaimTypes.Email)?.Value,
+                Username = principal.FindFirst(ClaimTypes.NameIdentifier)?.Value
+            };
+        }
+    }
+}
