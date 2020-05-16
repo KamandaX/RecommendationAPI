82aaaecfd68cb5dbca8b6923c32d33c41d52ba48
wizacass <visak.pet0@gmail.com>
2020-05-17 00:05:38 +0300

    Implement stub methods for signup and login

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using API.Models;
+using API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/v1/[controller]/[action]")]
+    [Produces(ApiContentType)]
+    [ApiController]
+    public class UsersController : ApiControllerBase
+    {
+        public UsersController(ApiContext context, Iserializer serializer, IErrorFormatter errorFormatter) :
+            base(context, serializer, errorFormatter) { }
+
+        [HttpPost]
+        public IActionResult Signup(User user)
+        {
+            if (!IsValidApiRequest())
+            {
+                return ApiBadRequest("Invalid headers!");
+            }
+
+            return NotImplementedError("Signup Not Implemented!");
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            if (!IsValidApiRequest())
+            {
+                return ApiBadRequest("Invalid headers!");
+            }
+
+            return NotImplementedError("Login Not Implemented!");
+        }
+    }
+}
---
+++ API/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class User
+    {
+        [Key] 
+        [JsonProperty("id")] 
+        public int ID { get; set; }
+
+        [Required] 
+        [JsonProperty("email")] 
+        public string Email { get; set; }
+
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [Required]
+        [JsonProperty("password")] 
+        public string Password { get; set; }
+    }
+}
