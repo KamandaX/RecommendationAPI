f8edfdb7eb28c4905c2e57eba2b5ed21f216fa2b
Gabrielius Laurinavičius <gabrielius9915@gmail.com>
2020-05-19 16:32:36 +0300

    Add ImageURL migration

--- API/Migrations/20200519132314_AddImageURL.Designer.cs
+++ API/Migrations/20200519132314_AddImageURL.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using API.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace API.Migrations
 {
     [DbContext(typeof(ApiContext))]
-    partial class ApiContextModelSnapshot : ModelSnapshot
+    [Migration("20200519132314_AddImageURL")]
+    partial class AddImageURL
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
