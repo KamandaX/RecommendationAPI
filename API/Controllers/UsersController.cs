597b77426367d1ec87e999c841af84c42aad0ad2
Gabrielius Laurinavičius <gabrielius9915@gmail.com>
2020-05-22 02:59:02 +0300

    Add signup logic

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,37 +1,59 @@
 ﻿using API.Models;
+using API.Models.AccountModels;
 using API.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
 
 namespace API.Controllers
 {
     [Route("api/v1/[controller]/[action]")]
     [Produces(ApiContentType)]
     [ApiController]
+    [Authorize]
     public class UsersController : ApiControllerBase
     {
-        public UsersController(ApiContext context, Iserializer serializer, IErrorFormatter errorFormatter) :
-            base(context, serializer, errorFormatter) { }
-
-        [HttpPost]
-        public IActionResult Signup(UserDTO user)
+        private readonly UserManager<UserDTO> _userManager;
+        private readonly SignInManager<UserDTO> _signInManager;
+        private readonly JwtService _jwt;
+        public UsersController(
+            ApiContext context,
+            Iserializer serializer,
+            IErrorFormatter errorFormatter,
+            IConfiguration config,
+            UserManager<UserDTO> userManager,
+            SignInManager<UserDTO> signInManager) :
+            base(context, serializer, errorFormatter)
         {
-            if (!IsValidApiRequest())
-            {
-                return ApiBadRequest("Invalid headers!");
-            }
-
-            return NotImplementedError("Signup Not Implemented!");
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _jwt = new JwtService(config);
         }
 
         [HttpPost]
-        public IActionResult Login(UserDTO user)
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterModel model)
         {
             if (!IsValidApiRequest())
             {
                 return ApiBadRequest("Invalid headers!");
             }
 
-            return NotImplementedError("Login Not Implemented!");
+            if (ModelState.IsValid)
+            {
+                var user = new UserDTO { UserName = model.Username ?? model.Email, Email = model.Email };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return Ok();
+                }
+
+                return BadRequest(result);
+            }
+
+            return BadRequest(ModelState);
         }
     }
 }
