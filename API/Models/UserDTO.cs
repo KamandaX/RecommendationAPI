e3b98343b6710b75a3b188d768ca22583f6d9a1f
Gabrielius Laurinavičius <gabrielius9915@gmail.com>
2020-05-22 02:53:41 +0300

    Change to IdentityUser

--- API/Models/UserDTO.cs
+++ API/Models/UserDTO.cs
@@ -1,28 +1,8 @@
-﻿using System;
-using Newtonsoft.Json;
-using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
 
 namespace API.Models
 {
-    public class UserDTO
+    public class UserDTO : IdentityUser
     {
-        [Key] 
-        [JsonProperty("id")] 
-        public int ID { get; set; }
-
-        [Required] 
-        [JsonProperty("email")] 
-        public string Email { get; set; }
-
-        [JsonProperty("username")]
-        public string Username { get; set; }
-
-        [Required]
-        [JsonProperty("password")] 
-        public string Password { get; set; }
-
-        [Required]
-        [JsonProperty("registered_at")]
-        public DateTime RegisteredAt { get; set; }
     }
 }
