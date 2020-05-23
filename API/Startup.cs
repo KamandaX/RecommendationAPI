c0c3dcb75fbc5ed1fe47f793fadc95877c1181b2
Gabrielius Laurinavičius <gabrielius9915@gmail.com>
2020-05-23 23:33:26 +0300

    Add user seed

--- API/Startup.cs
+++ API/Startup.cs
@@ -100,13 +100,13 @@ namespace API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApiContext context)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApiContext context, UserManager<UserDTO> userManager)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
 
-                var seed = new SeedData(context);
+                var seed = new SeedData(context, userManager);
                 seed.Seed();
             }
 
