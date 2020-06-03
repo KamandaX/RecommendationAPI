1bb7d5e2efe283f185ed28e545092c28eb3c8459
Gabrielius Laurinavičius <gabrielius9915@gmail.com>
2020-06-03 12:34:09 +0300

    Add quiz test

---
+++ API.Tests/QuizTest.cs
@@ -0,0 +1,33 @@
+﻿using API.Controllers;
+using API.Models;
+using API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace API.Tests
+{
+    public class QuizTest
+    {
+        [Fact]
+        public async Task GetQuestion_WithGoodHeaders_ReturnsGetQuestionDTO()
+        {
+            var options = new DbContextOptionsBuilder<ApiContext>()
+            .UseInMemoryDatabase("IN_MEMORY_DATABASE")
+            .Options;
+
+            var mockRepo = new Mock<ApiContext>(options);
+
+            QuestionsController controller = new QuestionsController(mockRepo.Object, new JsonSerializer(), new JsonErrorFormatter());
+            controller.ControllerContext = new ControllerContext();
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+            controller.ControllerContext.HttpContext.Request.Headers["X-Api-Request"] = "true";
+            var result = await controller.GetQuestion(0);
+
+            Assert.IsType<ActionResult<GetQuestionDTO>>(result);
+        }
+    }
+}
